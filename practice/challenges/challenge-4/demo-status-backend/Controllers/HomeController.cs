b14e6aa9875dc40e1129834aab411b44359cf95d
Marc Merzinger <marc@acloudjourney.io>
2021-09-10 10:25:58 +0200

    Add challenges

---
+++ practice/challenges/challenge-4/demo-status-backend/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace demo_status_backend.Controllers
+{
+    [ApiController]
+    [Route("/status")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return String.Concat("Greetings from ", Dns.GetHostName());
+        }
+    }
+}
---
+++ practice/challenges/challenge-4/demo-status-backend/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace demo_status_backend.Controllers
+{
+    [ApiController]
+    [Route("/status/who")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return String.Concat("Greetings from ", Dns.GetHostName());
+        }
+    }
+}
